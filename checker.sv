3063c562a3f6806a199989bb70adffc5b17c5d79
Caro1222 <110348521+Caro1222@users.noreply.github.com>
2024-09-04 16:19:11 -0600

    Update checker.sv

--- checker.sv
+++ checker.sv
@@ -58,6 +58,40 @@ class checker #(parameter width=16, parameter depth =8);
            emul_fifo.push_back(transaccion);
          end
        end
+       escritura_lectura: begin // si está vacía genera un underflow 
+        if(0 == emul_fifo.size()) begin
+          to_sb.tiempo_pop = transaccion.tiempo;
+             to_sb.underflow = 1;
+             to_sb.print("Checker: Underflow");
+             chkr_sb_mbx.put(to_sb);
+        end
+        else if(emul_fifo.size() == depth) begin // Revisa si la Fifo está llena para generar un overflow
+        auxiliar = emul_fifo.pop_front();
+           to_sb.dato_enviado = auxiliar.dato;
+           to_sb.tiempo_push = auxiliar.tiempo;
+           to_sb.overflow = 1;
+           to_sb.print("Checker: Overflow");
+           chkr_sb_mbx.put(to_sb);
+           emul_fifo.push_back(transaccion);
+        end  else begin  // En caso de no estar llena simplemente guarda el dato en la fifo simulada
+           transaccion.print("Checker: Escritura");
+           emul_fifo.push_back(transaccion);
+           auxiliar = emul_fifo.pop_front();
+           if(transaccion.dato == auxiliar.dato) begin
+             to_sb.dato_enviado = auxiliar.dato;
+             to_sb.tiempo_push = auxiliar.tiempo;
+             to_sb.tiempo_pop = transaccion.dato;
+             to_sb.completado = 1;
+             to_sb.calc_latencia();
+             to_sb.print("Checker:Transaccion Completada");
+             chkr_sb_mbx.put(to_sb);
+           end else begin
+             transaccion.print("Checker: Error el dato de la transacción no calza con el esperado");
+            $display("Dato_leido= %h, Dato_Esperado = %h",transaccion.dato,auxiliar.dato);
+            $finish; 
+           end
+         end
+       end
        reset: begin 
          contador_auxiliar = emul_fifo.size();
          for(int i =0; i<contador_auxiliar; i++)begin
